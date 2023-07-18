1b3aeb2468fa81768ef356aca8029022034184e0
Dmitri54 <dmitrimining@gmail.com>
2023-07-18 19:21:49 +0700

    sem_01  Square Complexity

--- lectures/sem_01/Program_01.java
+++ lectures/sem_01/Program_01.java
@@ -9,28 +9,65 @@
 
 package lectures.sem_01;
 
+import java.util.Random;
+
+// ==================================================================================================
+// Линейная - O(n);
+// public class Program_01 {
+//     public static void main (String[] args){
+//         int a = 5;
+
+//         System.out.println(sumN(a));
+//         System.out.println(sum(a));
+//     }
+
+//     public static int sumN (int n) { // Линейная сложность- O(n); Можно ли оптимизировать? (Через арифметическую прогрессию)
+//         int result = 0;
+//         for (int i = 1; i <= n; i++) 
+//             result += i;
+//         return result;
+        
+//     }
+
+//     public static int sum (int n) { // Арифметическая прогрессия
+
+//         return n * (n + 1) / 2;         
+//     }
+    
+// }
+
+// =========================================================================================
+// Квадратная - O(n^2).
 public class Program_01 {
     public static void main (String[] args){
-        int a = 5;
 
-        System.out.println(sumN(a));
-        System.out.println(sum(a));
-    }
+        int [] array = new int [1000];
+        for (int i = 0; i < array.length; i++) 
+            array[i] = new Random().nextInt(100);
 
-    public static int sumN (int n) { // Линейная сложность- O(n); Можно ли оптимизировать? (Через арифметическую прогрессию)
-        int result = 0;
-        for (int i = 1; i <= n; i++) 
-            result += i;
-        return result;
+        bubbleStort(array);
+        
+        for (int i : array)
+            System.out.println(i + " ");
         
     }
 
-    public static int sum (int n) { // Арифметическая прогрессия
+// Бобл сорт - если левый больше, тогда они меняются местами.
 
-        return n * (n + 1) / 2;         
+    public static void bubbleStort(int[] array){ // Квадратная сложность - O(n^2).
+        for (int k = 0; k < array.length - 1; k++) { // Повтор алгоритма перемещениея.
+            for (int i = 0; i < array.length - 1; i++) { // Тут само перемещение элемента.
+                    if (array[i] > array[i + 1]) {
+                        int temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                }              
+            }           
+        }
     }
     
 }
+
 
 
 /**
