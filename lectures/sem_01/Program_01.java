8a9cbf6259b33e4fdd65ae0573b4334778f4498c
Dmitri54 <dmitrimining@gmail.com>
2023-07-19 14:03:57 +0700

    sem_01, added Recursion

--- lectures/sem_01/Program_01.java
+++ lectures/sem_01/Program_01.java
@@ -38,100 +38,125 @@ import java.util.Random;
 
 // =========================================================================================
 // Квадратная - O(n^2).
+// public class Program_01 {
+//     public static void main (String[] args){
+
+//         int [] array = new int [10000];
+//         for (int i = 0; i < array.length; i++) 
+//             array[i] = new Random().nextInt(100);
+
+// // Создал массив array2, чтобы проверить работоспособность функции bubbleStort3,
+// // т.к. array получался уже отсортированным, ответ был 0, что не верно.
+
+//         int [] array2 = new int [10000];
+//         for (int i = 0; i < array2.length; i++) 
+//             array2[i] = new Random().nextInt(100);
+
+//         // long timeStart1 = System.currentTimeMillis(); 
+//         // bubbleStort(array);
+//         // long timeEnd1 = System.currentTimeMillis(); 
+
+//         long timeStart2 = System.currentTimeMillis(); 
+//         bubbleStort2(array);
+//         long timeEnd2 = System.currentTimeMillis(); 
+
+//         long timeStart3 = System.currentTimeMillis(); 
+//         bubbleStort3(array2);
+//         long timeEnd3 = System.currentTimeMillis(); 
+
+//         // System.out.println(timeEnd1 - timeStart1);
+//         System.out.println(timeEnd2 - timeStart2);
+//         System.out.println(timeEnd3 - timeStart3);
+
+//         // for (int i : array) // Актуально при рассмотрении функции bubbleStort.
+//         //     System.out.print(i + " ");
+        
+//     }
+
+// // Бобл сорт - если левый больше, тогда они меняются местами.
+
+//     public static void bubbleStort(int[] array){ // Квадратная сложность - O(n^2).
+//         for (int k = 0; k < array.length - 1; k++) { // Повтор алгоритма перемещениея.
+//             for (int i = 0; i < array.length - 1; i++) { // Тут само перемещение элемента.
+//                 if (array[i] > array[i + 1]) {
+//                     int temp = array[i];
+//                     array[i] = array[i + 1];
+//                     array[i + 1] = temp;
+//                 }              
+//             }           
+//         }
+//     }
+
+//     // Как изменить количество шагов, чтобы функция bubbleStort работала быстрее?
+//     // Оптимизирую 
+
+//     public static void bubbleStort2(int[] array){ 
+//         int length = array.length - 1; //  Убрал повторную проверку элемента.
+//         for (int k = 0; k < array.length - 1; k++) { 
+//             for (int i = 0; i < array.length - 1; i++) { 
+//                 if (array[i] > array[i + 1]) {
+//                     int temp = array[i];
+//                     array[i] = array[i + 1];
+//                     array[i + 1] = temp;
+//                 }              
+//             }
+//             length--; // Сократил длинну списка на один последний элемент, при каждом следующем шаге.           
+//         }
+        
+//     }
+//     // Сложность получается ниже: 
+//     // O(n * (n - 1) / 2)
+//     // Если раскрыть и убрать 2, т.к. константа у нас не учитывается при подсчете сложностей.
+//     // O(n^2 - n)   - получается вот так и надо не забывать, что все делиться на 2.
+
+// // -----------------------------------------------------------------------------------------------
+// // Можно ещё оптимизировать, ускорев работу bubbleStort2.
+
+//     public static void bubbleStort3(int[] array){ 
+//         int length = array.length - 1; 
+//         boolean flag = true; // Ограничет проверку уже отсортированного массива. 
+//         for (int k = 0; k < array.length - 1 && flag; k++) { 
+//             flag = false;
+//             for (int i = 0; i < array.length - 1; i++) { 
+//                 if (array[i] > array[i + 1]) {
+//                     flag = true;
+//                     int temp = array[i];
+//                     array[i] = array[i + 1];
+//                     array[i + 1] = temp;
+//                 }              
+//             }
+//             length--;          
+//         } 
+//     }
+// // Но в расчете сложности работа flag не будет учитываться, 
+// // т.к. при расчете сложности берется самым худжий вариант растановки элементов.
+// }
+
+// ===========================================================================================
+// Рекурсия (recursion)
+
 public class Program_01 {
     public static void main (String[] args){
+        int a = 20;
 
-        int [] array = new int [10000];
-        for (int i = 0; i < array.length; i++) 
-            array[i] = new Random().nextInt(100);
-
-// Создал массив array2, чтобы проверить работоспособность функции bubbleStort3,
-// т.к. array получался уже отсортированным, ответ был 0, что не верно.
-
-        int [] array2 = new int [10000];
-        for (int i = 0; i < array2.length; i++) 
-            array2[i] = new Random().nextInt(100);
-
-        // long timeStart1 = System.currentTimeMillis(); 
-        // bubbleStort(array);
-        // long timeEnd1 = System.currentTimeMillis(); 
-
-        long timeStart2 = System.currentTimeMillis(); 
-        bubbleStort2(array);
-        long timeEnd2 = System.currentTimeMillis(); 
-
-        long timeStart3 = System.currentTimeMillis(); 
-        bubbleStort3(array2);
-        long timeEnd3 = System.currentTimeMillis(); 
-
-        // System.out.println(timeEnd1 - timeStart1);
-        System.out.println(timeEnd2 - timeStart2);
-        System.out.println(timeEnd3 - timeStart3);
-
-        // for (int i : array) // Актуально при рассмотрении функции bubbleStort.
-        //     System.out.print(i + " ");
-        
+        long startTime1 = System.nanoTime();
+        System.out.println(fib(a));
+        long endTime1 = System.nanoTime();
     }
 
-// Бобл сорт - если левый больше, тогда они меняются местами.
-
-    public static void bubbleStort(int[] array){ // Квадратная сложность - O(n^2).
-        for (int k = 0; k < array.length - 1; k++) { // Повтор алгоритма перемещениея.
-            for (int i = 0; i < array.length - 1; i++) { // Тут само перемещение элемента.
-                if (array[i] > array[i + 1]) {
-                    int temp = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = temp;
-                }              
-            }           
-        }
+    public static int fib(int n){ // Стандартная рекурсия для Фибоначи, но она медленная.
+        if (n < 1)
+            return -1;
+        if (n == 1)
+            return 0;
+        if (n <= 3)
+            return 1;
+        return fib(n - 1) + fib(n - 2);
     }
-
-    // Как изменить количество шагов, чтобы функция bubbleStort работала быстрее?
-    // Оптимизирую 
-
-    public static void bubbleStort2(int[] array){ 
-        int length = array.length - 1; //  Убрал повторную проверку элемента.
-        for (int k = 0; k < array.length - 1; k++) { 
-            for (int i = 0; i < array.length - 1; i++) { 
-                if (array[i] > array[i + 1]) {
-                    int temp = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = temp;
-                }              
-            }
-            length--; // Сократил длинну списка на один последний элемент, при каждом следующем шаге.           
-        }
-        
-    }
-    // Сложность получается ниже: 
-    // O(n * (n - 1) / 2)
-    // Если раскрыть и убрать 2, т.к. константа у нас не учитывается при подсчете сложностей.
-    // O(n^2 - n)   - получается вот так и надо не забывать, что все делиться на 2.
-
-// -----------------------------------------------------------------------------------------------
-// Можно ещё оптимизировать, ускорев работу bubbleStort2.
-
-    public static void bubbleStort3(int[] array){ 
-        int length = array.length - 1; 
-        boolean flag = true; // Ограничет проверку уже отсортированного массива. 
-        for (int k = 0; k < array.length - 1 && flag; k++) { 
-            flag = false;
-            for (int i = 0; i < array.length - 1; i++) { 
-                if (array[i] > array[i + 1]) {
-                    flag = true;
-                    int temp = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = temp;
-                }              
-            }
-            length--;          
-        } 
-    }
-// Но в расчете сложности работа flag не будет учитываться, 
-// т.к. при расчете сложности берется самым худжий вариант растановки элементов.
-
 }
+
+
+
 
 
 
