bfea16804158e5d80a2f260d74f1680b49eaa84c
Dmitri54 <dmitrimining@gmail.com>
2023-07-25 19:44:14 +0700

    sem_03 Added method remove, removeAt, removeAll, getNode, getValue, swap

--- lectures/sem_03/LinkedList.java
+++ lectures/sem_03/LinkedList.java
@@ -10,13 +10,14 @@ public class LinkedList { // Связанный список
             size = 1; 
             return;
         }
-// Добавление в конец
+// Добавление в конец.
         Node currentNode = root; // Начинаю с первой ссылки
         while (currentNode.next != null) // запускаю цикл (пока следующий элемент не равен null)
             currentNode = currentNode.next; // Перезаписываю на новое значение пока не закончится цикл.
         currentNode.next = new Node(value); // Записываю новую Node, т.е. ссылку на последний злемент.
         size++;
     }
+
 // Удаление по значению
     public boolean remove(int value){
         if (root == null) // Если равен нулю возвращаю false.
@@ -38,6 +39,88 @@ public class LinkedList { // Связанный список
         return false; // Если значение так и не было найдено.
         
     }
+
+    public void removeAt(int index){ // Удаление по индексу
+        if (index < 0 || index >= size) // Если вышел за границы списка.
+            throw new IndexOutOfBoundsException();
+        if (index == 0) { // Если удаляю первый элемент.
+            root = root.next; 
+            size--;
+            return;
+        }
+        Node preDeleted = this.getNode(index - 1); // Беру Node предыдущего и 
+        preDeleted.next = preDeleted.next.next; // в следующую Node записываю значение Node после удаляемого Node.
+        size--;
+
+    }
+
+    public int removeAll(int value){ // Удаление всех элементов по значению
+        int prevSize = size; // Чтобы можно было посмотреть сколько элементов удалил.
+        while (root != null && root.value == value){
+            root = root.next; // Удалит первые элементы если есть совпадение
+            size--; 
+        }
+        if (root == null)
+            return prevSize - size;
+            
+        Node currentNode = root;
+        while (currentNode.next != null) {
+            if (currentNode.next.value == value) { // Удалит все совпадения по значению
+                currentNode.next = currentNode.next.next; 
+                size--;
+            } else 
+                currentNode = currentNode.next;
+        }
+        return prevSize - size;
+        
+    }
+
+// Вспомогательные методы getNode и getValue, setValue, swap:
+    private Node getNode(int index){ // Будет вытаскивать нужную Node по index
+        if (root == null)
+            return null;
+        if (index < 0 || index >= size) // Проверяю, что не вышел за пределы списка
+            throw new IndexOutOfBoundsException();
+        Node currentNode = root; // Начитаю с первой ссылки
+        for (int i = 0; i < index; i++) { // Цикл до значения index
+            currentNode = currentNode.next; // Сохраняю найденный Node
+        }
+        return currentNode; // Нашел Node по index.
+    }
+
+    public int getValue(int index) { // Будет вытаскивать значение по index.
+        Node value = this.getNode(index); // Записал значение Node найденную по index в переменную
+        return value.value; // Нашел value по index.
+
+        // return this.getNote(index).value; // Так короче.
+    }
+
+// Вставка значения по index
+    public void setValue(int index, int value) { // Вставка значения по index (забираю Node и меняю значение).
+        this.getNode(index).value = value;
+    }
+
+    public void swap(int index1, int index2){ // Метод будет брать два index и менять значение Node.
+        if (index1 < 0 || index1 >= size || index2 < 0 || index2 >= size)
+            return;
+            // throw new IndexOutOfBoundsException(); // Можно так написать, тогда будет видно, что за ошибка.
+        if (index1 == index2)
+            return;
+        Node node1 = null, node2 = null, currentNode = root;
+        for (int i = 0; currentNode != null; i++) {
+            if (index1 == i)
+                node1 = currentNode;
+            else if (index2 == i)
+                node2 = currentNode;
+            if (node2 != null && node1 != null)
+                break;
+            currentNode = currentNode.next; // Перехожу к следующему
+        }
+        int temp = node1.value; // меняю местами значения.
+        node1.value = node2.value;
+        node2.value = temp;
+    }
+
 
 // Метод для вывода в консоль
     public void print(){ 
--- lectures/sem_03/Program.java
+++ lectures/sem_03/Program.java
@@ -10,15 +10,30 @@ public class Program {
         list.print();
 
         list.add(5);
+        list.add(5);
+        list.add(5);
         list.add(8);
         list.add(1);
         list.add(7);
-        list.add(2);
-        list.add(4);
+        list.add(5);
+        list.add(5);
 
         list.print(); // [ 5 8 1 7 2 4 ] size: 6
         list.remove(1);
         list.print(); // [ 5 8 7 2 4 ] size: 5
+
+        System.out.println(list.getValue(2));
+
+        list.setValue(0, 2);
+        list.print();
+        
+
+        // list.removeAll(5);
+        int n = list.removeAll(5);
+        list.print();
+        System.out.println(n);
+
+
         
     }
     
