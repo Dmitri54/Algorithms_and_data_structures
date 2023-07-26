100978255ac4739ea0aa237515cb80d97da1b0f4
Dmitri54 <dmitrimining@gmail.com>
2023-07-26 17:39:40 +0700

    sem_03 class and method Compare

--- lectures/sem_03/Program.java
+++ lectures/sem_03/Program.java
@@ -6,9 +6,42 @@ package lectures.sem_03;
 
 public class Program {
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
+        // LinkedList list = new LinkedList();
         
-        list.print();
+        // list.print();
+
+        // list.add(5);
+        // list.add(5);
+        // list.add(5);
+        // list.add(8);
+        // list.add(1);
+        // list.add(7);
+        // list.add(5);
+        // list.add(5);
+        
+        // // LinkedList list2 = list; // Так копировать не будет!!!
+
+        // LinkedList list2 = list.copyList();
+
+        // list.print(); // [ 5 8 1 7 2 4 ] size: 6
+        // list.remove(1);
+        // list.print(); // [ 5 8 7 2 4 ] size: 5
+
+        // System.out.println(list.getValue(2));
+
+        // list.setValue(0, 2);
+        // list.print();
+        
+
+        // // list.removeAll(5);
+        // int n = list.removeAll(5);
+        // list.print();
+        // System.out.println(n); // 4
+        
+        // System.out.print("list2: ");
+        // list2.print(); // [ 5 5 5 8 1 7 5 5 ] size: 8
+// -------------------------------------------------------------------------------------------------
+        LinkedListT<Integer> list = new LinkedListT<>(); // Создаю джинерика с типом данных Integer.
 
         list.add(5);
         list.add(5);
@@ -18,31 +51,50 @@ public class Program {
         list.add(7);
         list.add(5);
         list.add(5);
-        
-        // LinkedList list2 = list; // Так копировать не будет!!!
 
-        LinkedList list2 = list.copyList();
-
-        list.print(); // [ 5 8 1 7 2 4 ] size: 6
-        list.remove(1);
-        list.print(); // [ 5 8 7 2 4 ] size: 5
-
-        System.out.println(list.getValue(2));
-
-        list.setValue(0, 2);
+        System.out.print("list: ");
         list.print();
+        LinkedListT<Integer> list2 = list.copyList();
         
 
-        // list.removeAll(5);
-        int n = list.removeAll(5);
-        list.print();
-        System.out.println(n); // 4
-        
-        System.out.print("list2: ");
-        list2.print(); // [ 5 5 5 8 1 7 5 5 ] size: 8
 
+// ----------------------------------------------------------------------------------------------------
+        // Vector a = new Vector(2, 3, 4);
+        // Vector b = new Vector(2, 9, 1);
 
-        
+        // // if (a > b) { // Если не прописать метод compareTo, будет ошибка. Компилятор не поймет по какому значению сравнивать!
+        // if (a.compareTo(b) > 0) { // a > b
+        //     System.out.println("Bigger");
+        // }
+        // if (a.compareTo(b) < 0) { // a < b
+        //     System.out.println("Smaller");
+        // }
+        // if (a.compareTo(b) == 0) { // a == b
+        //     System.out.println("Equals");
+        // }
     }
     
 }
+
+// --------------------------------------------------------------------------------------------------
+// class Vector {
+//     int x;
+//     int y;
+//     int z;
+
+//     Vector(int x, int y, int z){
+//         this.x = x;
+//         this.y = y;
+//         this.z = z;
+//     }
+// // Сомпоратор (Compare) - суть сравнивать экземпляры классов.
+//     public int compareTo(Vector vector) { // Данный метод позволяет определить критерий для сравнения.
+//         if (this.length() > vector.length())
+//             return 1;
+//         if (this.length() < vector.length())
+//             return -1;
+//         return 0;
+//     }
+// // Получение длинну текущего вектора
+//     public double length() {return Math.sqrt(x * x + y * y + z * z);}
+// }
