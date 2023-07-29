428cab45ba5b265edc1c1a17f88d934b5cebf0d9
Dmitri54 <dmitrimining@gmail.com>
2023-07-29 19:26:42 +0700

    les_04 Method add

--- les_04/RedBlackTree.java
+++ les_04/RedBlackTree.java
@@ -2,7 +2,21 @@ package les_04;
 
 public class RedBlackTree { // Красно-черное дерево
 
-    private Node node;
+    private Node root;
+
+    public boolean add(int value){
+        if (root != null){ // Если root Node уже существует
+            boolean result = addNode(root, value);
+            root = rebalance(root); // вызываю балансировку
+            root.color = Color.BLACK; // ставлю цвет BLACK
+            return result;
+        } else { // Иначе
+            root = new Node(); // создаю нового root Node
+            root.color = Color.BLACK;
+            root.value = value;
+            return true;
+        }
+    }
 
     private boolean addNode(Node node, int value){ // Добавление Node
         if (node.value == value){
@@ -39,24 +53,24 @@ public class RedBlackTree { // Красно-черное дерево
         boolean needRebalance;
         do {
             needRebalance = false;
-            if (result.rightChild != null && result.rightChild.color == Color.RED &&
-                    (result.leftChild == null || result.leftChild.color == Color.BLACK)) {
-                needRebalance = true;
-                result = rightSwap(result);
+            if (result.rightChild != null && result.rightChild.color == Color.RED && // есть правый ребенок и у него цвет RED
+                    (result.leftChild == null || result.leftChild.color == Color.BLACK)) { // и левый ребенок имеет цвет BLACK 
+                needRebalance = true; // говорю, что needRebalance нужно повторить
+                result = rightSwap(result); // Делаю правый поворот
             }
-            if (result.leftChild != null && result.leftChild.color == Color.RED &&
-                    result.leftChild.leftChild == null && result.leftChild.leftChild.color == Color.RED){
+            if (result.leftChild != null && result.leftChild.color == Color.RED && // если левый ребенок и его цвет RED
+                    result.leftChild.leftChild == null && result.leftChild.leftChild.color == Color.RED){ // и у этого левого ребенка есть левый ребенок с цветом RED
                 needRebalance = true;
-                result = leftSwap(result);
+                result = leftSwap(result); // делаю левый поворот
             }
-            if (result.leftChild != null && result.leftChild.color == Color.RED && 
-                    result.rightChild != null && result.rightChild.color == Color.RED){
+            if (result.leftChild != null && result.leftChild.color == Color.RED && //  если левый ребенок и
+                    result.rightChild != null && result.rightChild.color == Color.RED){ // правый ребенок имееют цвет RED
                 needRebalance = true;
-                colorSwap(node);
+                colorSwap(node); // делаю смену цвета
             }
-        }
+        } // выйду из цикла когда условия перестанут исполняться
         while (needRebalance);
-        return result;
+        return result; // верну результат.
         
     }
 
