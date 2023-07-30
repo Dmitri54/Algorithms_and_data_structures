95df3fce48e7cf582e27a8827a5be22075058a3a
Dmitri54 <dmitrimining@gmail.com>
2023-07-30 20:19:48 +0700

    sem_04 checking the method add (without balancing)

--- lectures/sem_04/BinaryTree.java
+++ lectures/sem_04/BinaryTree.java
@@ -37,7 +37,7 @@ public class BinaryTree<T extends Comparable<T>> {
         }
         return addNode(node.right, value);
     }
- 
+
     enum Color {red, black}
 
     private class Node{
--- lectures/sem_04/Program.java
+++ lectures/sem_04/Program.java
@@ -84,6 +84,15 @@ import java.util.Random;
 
 public class Program {
     public static void main(String[] args) {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+
+        tree.add(5);
+        tree.add(9);
+        tree.add(8);
+        tree.add(7);
+        tree.add(6);
         
+        tree.print(); // Вижу дерево, не очень красиво, но все же. Пока без балансировки.
+
     }
 }
