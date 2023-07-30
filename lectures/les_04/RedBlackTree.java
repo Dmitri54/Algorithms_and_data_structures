a7243d48e7e4de6c27609fbca528693f13da79be
Dmitri54 <dmitrimining@gmail.com>
2023-07-30 15:35:47 +0700

    Moved the folder les_04

---
+++ lectures/les_04/RedBlackTree.java
@@ -0,0 +1,121 @@
+package les_04;
+
+public class RedBlackTree { // Красно-черное дерево
+
+    private Node root;
+
+    public boolean add(int value){
+        if (root != null){ // Если root Node уже существует
+            boolean result = addNode(root, value);
+            root = rebalance(root); // вызываю балансировку
+            root.color = Color.BLACK; // ставлю цвет BLACK
+            return result;
+        } else { // Иначе
+            root = new Node(); // создаю нового root Node
+            root.color = Color.BLACK;
+            root.value = value;
+            return true;
+        }
+    }
+
+    private boolean addNode(Node node, int value){ // Добавление Node
+        if (node.value == value){
+            return false;
+        } else {
+            if (node.value > value){
+                if (node.leftChild != null) {
+                    boolean result = addNode(node.leftChild, value);
+                    node.leftChild = rebalance(node.leftChild);
+                    return result;
+                } else {
+                    node.leftChild = new Node();
+                    node.leftChild.color = Color.RED;
+                    node.leftChild.value = value;
+                    return true;
+                }      
+            } else {
+                if (node.rightChild != null){
+                    boolean result = addNode(node.rightChild, value);
+                    node.rightChild = rebalance(node.rightChild);
+                    return result;
+                } else {
+                    node.rightChild = new Node();
+                    node.rightChild.color = Color.RED;
+                    node.rightChild.value = value;
+                    return true;
+                }
+            }
+        }
+    }
+
+    private Node rebalance(Node node) {
+        Node result = node;
+        boolean needRebalance;
+        do {
+            needRebalance = false;
+            if (result.rightChild != null && result.rightChild.color == Color.RED && // есть правый ребенок и у него цвет RED
+                    (result.leftChild == null || result.leftChild.color == Color.BLACK)) { // и левый ребенок имеет цвет BLACK 
+                needRebalance = true; // говорю, что needRebalance нужно повторить
+                result = rightSwap(result); // Делаю правый поворот
+            }
+            if (result.leftChild != null && result.leftChild.color == Color.RED && // если левый ребенок и его цвет RED
+                    result.leftChild.leftChild == null && result.leftChild.leftChild.color == Color.RED){ // и у этого левого ребенка есть левый ребенок с цветом RED
+                needRebalance = true;
+                result = leftSwap(result); // делаю левый поворот
+            }
+            if (result.leftChild != null && result.leftChild.color == Color.RED && //  если левый ребенок и
+                    result.rightChild != null && result.rightChild.color == Color.RED){ // правый ребенок имееют цвет RED
+                needRebalance = true;
+                colorSwap(node); // делаю смену цвета
+            }
+        } // выйду из цикла когда условия перестанут исполняться
+        while (needRebalance);
+        return result; // верну результат.
+        
+    }
+
+    private Node rightSwap(Node node) { // Правосторонний перевод
+        Node rightChild = node.rightChild;
+        Node betweenChild = rightChild.leftChild;
+        rightChild.leftChild = node;
+        node.rightChild = betweenChild;
+        rightChild.color = node.color;
+        node.color = Color.RED;
+        return rightChild;
+    }
+
+    private Node leftSwap(Node node) { // Левосторонний перевод
+        Node leftChild = node.leftChild; // Беру левого ребенка
+        Node betweenChild = leftChild.rightChild; // Беру промежуточного ребенка, для смены родителей местами
+        leftChild.rightChild = node; // текущий родитель
+        node.leftChild = betweenChild; // 
+        leftChild.color = node.color;
+        node.color = Color.RED;
+        return leftChild;
+    }
+
+    private void colorSwap(Node node){ // Смена цвета
+        node.rightChild.color = Color.BLACK; //Беру детей, присваиваю им цвета
+        node.leftChild.color = Color.BLACK;
+        node.color = Color.RED; // а сам он становиться красным. 
+    }
+
+    private class Node{
+        private int value; // Значение в узле
+        private Color color;
+        private Node leftChild;
+        private Node rightChild;
+
+        @Override
+        public String toString(){
+            return "Node{" + 
+                    "value=" + value + 
+                    ", color=" + color + 
+                    "}";
+        }
+    }
+
+    private enum Color{
+        RED, BLACK
+    }
+}
---
+++ lectures/les_04/Tree.java
@@ -0,0 +1,56 @@
+package les_04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tree { // Дерево
+    Node root;
+
+// Обход в глубину (рекурсия)
+//     public boolean exist(int value){ // Публичный вид функции find
+//         if (root != null) { // Проверка на наличие root, т.к. дерево может быть пустым
+//             Node node = find(root, value);
+//             if(node != null){ // Если значение найдено
+//                 return true; // возвращаю true
+//             }
+//         }
+//         return false; // Если не найдено false.
+//     }
+//     private Node find (Node node, int value){
+//         if (node.value == value){ // Проверяю текущую Node, если в Node есть нужное значение
+//             return node; // возвращаю Node
+//         } else { // Если значения нет
+//             for (Node child : node.children) { // перебераю его детей
+//                 Node result = find(child, value); // для каждого ребенка рекурсивно запускается поиск.
+//                 if (result != null){
+//                     return result;
+//                 }
+//             }
+//         }
+//         return null; // Если не найдено значение.
+//     }
+// -------------------------------------------------------------------------------------
+// Обход в ширину (циклы)
+    private Node find(int value){
+        List<Node> line = new ArrayList<>(); // Создаю список
+        line.add(root); // Добавляю root
+        while (line.size() > 0){ // Пока в моём объекте существуют дети делаю
+            List<Node> nexttLine = new ArrayList<>(); // Создаю список
+            for (Node node : line) { // перебераю линию, для каждого элемента
+                if (node.value == value){
+                    return node; // возвращаю Node
+                }
+                nexttLine.addAll(node.children); // Если не найдено, добавляю всех его детей
+            }
+            line = nexttLine; // нет вхождений в цикл, добавляю линию и перезапускаю цикл.
+        }
+        return null;
+    }
+
+
+    public class Node{
+        int value; // Значение
+        List<Node> children; // Список детей
+    }
+}
+
